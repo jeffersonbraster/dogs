d6c93de4a93c3e4359ca4cc72bd6e06dd0f6d646
Jefferson <jeffersonbraster@gmail.com>
2021-01-02 18:30:55 -0300

    criando rotas protegidas

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
 import Footer from "./Components/Footer";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 
 import { UserStorage } from "./UserContext";
 
@@ -17,7 +19,8 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
