cf3c2be7cbb0c8c10fff32a20b6389b17f2ef85a
Jefferson <jeffersonbraster@gmail.com>
2021-01-02 00:50:54 -0300

    validate token

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { TOKEN_POST, USER_GET } from "./api";
+import { useNavigate } from "react-router-dom";
+import { TOKEN_POST, USER_GET, TOKEN_VALIDADE_POST } from "./api";
 
 export const UserContext = React.createContext();
 
@@ -8,6 +9,17 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(false);
+
+  const navigate = useNavigate();
+
+  const userLogout = React.useCallback(async function () {
+    setData(null);
+    setError(null);
+    setLoading(false);
+    setLogin(false);
+    window.localStorage.removeItem("DOGS-Token");
+    navigate("/login");
+  }, []);
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
@@ -19,15 +31,50 @@ export const UserStorage = ({ children }) => {
   }
 
   async function userLogin(username, password) {
-    const { url, options } = TOKEN_POST({ username, password });
-    const tokenRes = await fetch(url, options);
-    const { token } = await tokenRes.json();
-    window.localStorage.setItem("DOGS-Token", token);
-    getUser(token);
+    try {
+      setError(null);
+      setLoading(true);
+      const { url, options } = TOKEN_POST({ username, password });
+      const tokenRes = await fetch(url, options);
+      if (!tokenRes.ok) throw new Error(`Error: usuário invalido.`);
+      const { token } = await tokenRes.json();
+      window.localStorage.setItem("DOGS-Token", token);
+      await getUser(token);
+      navigate("/conta");
+    } catch (err) {
+      setError(err.message);
+      setLogin(false);
+    } finally {
+      setLoading(false);
+    }
   }
 
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem("DOGS-Token");
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDADE_POST(token);
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error("Token invalido.");
+          await getUser(token);
+        } catch (error) {
+          userLogout();
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+
+    autoLogin();
+  }, [userLogout]);
+
   return (
-    <UserContext.Provider value={{ userLogin, data }}>
+    <UserContext.Provider
+      value={{ userLogin, data, userLogout, error, loading, login }}
+    >
       {children}
     </UserContext.Provider>
   );
