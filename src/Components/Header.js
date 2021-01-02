69cc87051bd72214d2eee246d82c4029a1207393
Jefferson <jeffersonbraster@gmail.com>
2021-01-01 22:20:52 -0300

    criando Usercontexto

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,16 +4,26 @@ import styles from "./Header.module.css";
 
 import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
+import { UserContext } from "../UserContext";
+
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link className={styles.login} to="/login">
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            Bem vindo, {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
