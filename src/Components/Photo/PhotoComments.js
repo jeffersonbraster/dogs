a776d66c021fb3f5c92862540f0a1c07fd65357b
Jefferson <jeffersonbraster@gmail.com>
2021-01-06 17:19:37 -0300

    comentarios post

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,7 +1,25 @@
 import React from "react";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import { UserContext } from "../../UserContext";
 
-const PhotoComments = () => {
-  return <div></div>;
+import styles from "./PhotoComments.module.css";
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const { login } = React.useContext(UserContext);
+  return (
+    <>
+      <ul className={styles.comment}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}</b>:{" "}
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
