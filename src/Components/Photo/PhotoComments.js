e8a83c49043290b828d40e551d70f37c08423e47
Jefferson <jeffersonbraster@gmail.com>
2021-01-06 21:27:41 -0300

    adicionando estilos no comentarios

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -6,10 +6,16 @@ import styles from "./PhotoComments.module.css";
 
 const PhotoComments = (props) => {
   const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSections = React.useRef(null);
   const { login } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    commentsSections.current.scrollTop = commentsSections.current.scrollHeight;
+  }, [comments]);
+
   return (
     <>
-      <ul className={styles.comment}>
+      <ul ref={commentsSections} className={styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
             <b>{comment.comment_author}</b>:{" "}
--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -3,6 +3,7 @@ import useFetch from "../../Hooks/useFetch";
 import { COMMENT_POST } from "../../api";
 import { ReactComponent as Enviar } from "../../Assets/enviar.svg";
 import Error from "../Helper/Error";
+import styles from "./PhotoCommentsForm.module.css";
 
 const PhotoCommentsForm = ({ id, setComments }) => {
   const { request, error } = useFetch();
@@ -19,15 +20,16 @@ const PhotoCommentsForm = ({ id, setComments }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         name="comment"
         id="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
       <Error error={error} />
