f6c506497a1eb67d9026c96209581bd1353cee6a
Jefferson <jeffersonbraster@gmail.com>
2021-01-09 22:15:25 -0300

    criando o footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs-footer.svg";
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <h1>Footer</h1>
-    </div>
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs - Criado com amor por Jefferson Brandão.</p>
+    </footer>
   );
 };
 
